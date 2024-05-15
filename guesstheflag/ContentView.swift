68fafd6d363efd7847002d1be30c0943bf289de2
Neeta Buhecha <neeta.buhecha@natwest.com>
2024-05-15 08:39:46 +0100

    Initial Commit

---
+++ guesstheflag/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  guesstheflag
+//
+//  Created by Buhecha, Neeta (Trainee Engineer) on 15/05/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ guesstheflag/guesstheflagApp.swift
@@ -0,0 +1,17 @@
+//
+//  guesstheflagApp.swift
+//  guesstheflag
+//
+//  Created by Buhecha, Neeta (Trainee Engineer) on 15/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct guesstheflagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
